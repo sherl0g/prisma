73df6c2c8519335ebea8c5df9c93c070e80a9c78
Bruce Lampson <brucelampson@gmail.com>
2020-10-28 22:28:52 -0600

    add new e2e tests

--- tests/e2e/specs/dashboard.js
+++ tests/e2e/specs/dashboard.js
@@ -1,37 +1,52 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('Dashboard test', () => {
-  it('Check for title value', () => {
+  it('check for title value is @sherlog/prisma', () => {
     cy.visit('/');
     cy.title().should('eq', '@sherlog/prisma');
   });
 
-  it('Dashboard default background color is white', () => {
+  it('dashboard default background color is white', () => {
     cy.visit('/');
     cy.get('body').should('have.css', 'background-color', 'rgb(255, 255, 255)');
   });
 
-  it('slider-track  is visible', () => {
+  it('play button is visible', () => {
+    cy.visit('/');
+    cy.get('.btn-play').should('be.visible');
+  });
+
+  it('slider track  is visible', () => {
     cy.visit('/');
     cy.get('.vue-slider').should('be.visible');
   });
 
-  it('slider-thumb is visible', () => {
+  it('slider thumb is visible', () => {
     cy.visit('/');
     cy.get('.vue-slider-dot-handle').should('be.visible');
   });
 
-  it('slider-track has initial state', () => {
+  it('slider track has initial state', () => {
     cy.visit('/');
     cy.get('.vue-slider-rail').should('have.css', 'background-color', 'rgb(204, 204, 204)');
   });
 
-  it('Duration is 0:00/0:00', () => {
+  it('duration contains 0:00/0:00', () => {
     cy.visit('/');
     cy.get('.duration').contains('0:00/0:00');
   });
 
-  it('Clicking the logo should toggle themes', () => {
+  it('zoom button is hidden', () => {
+    cy.visit('/');
+    cy.get('.btn-zoom').should('be.hidden');
+  });
+
+  it('status message contains text "connecting..."', () => {
+    cy.visit('/');
+    cy.get('.status-message').contains('connecting...');
+  });
+
+  it('clicking the logo should toggle themes', () => {
     cy.visit('/');
     cy.get('.logo').click();
     cy.get('body.darkTheme').should('have.css', 'background-color', 'rgb(41, 41, 41)');
@@ -39,3 +54,10 @@ describe('Dashboard test', () => {
     cy.get('body').should('have.css', 'background-color', 'rgb(255, 255, 255)');
   });
 });
+
+// TODO test zoom button is visible
+// TODO test play button is hidden while track is playing
+// TODO test play button renders logs and charts
+// TODO test pause button is visible while track is playing
+// TODO test pause button is hidden after clicking it
+// TODO test duration no longer contains 0:00/0:00
