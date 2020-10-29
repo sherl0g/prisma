648daf041bf16569ee54098d407d5347d87ec7ab
Bruce Lampson <brucelampson@gmail.com>
2020-10-28 22:34:55 -0600

    Add min-width to datetime column

--- src/components/dashboard/Item.vue
+++ src/components/dashboard/Item.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="item-inner">
     <div class="d-flex event" style="min-height: 50px">
-      <div class="p-2 flex-shrink-1 border-right" style="width: 190px">{{ source.datetime }}</div>
+      <div class="p-2 flex-shrink-1 border-right" style="min-width: 170px">{{ source.datetime }}</div>
       <div class="p-2 w-100">{{ source.event }}</div>
     </div>
   </div>
